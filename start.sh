e9f53de0e4ed1500fd000cfe749c404d68aa352b
root <root@vps.server.local>
2023-03-11 04:28:14 +0000

    vps

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
-gunicorn src:app --bind 82.180.173.44:80  --timeout 500 --threads 3 --workers 3 --reload
+gunicorn src:app --bind 82.180.173.44:8080  --timeout 500 --threads 3 --workers 3 --reload
 
